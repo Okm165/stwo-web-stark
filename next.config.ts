6b78dd46860f07af61cf51218d746696d01382b3
BartolomeoDiaz <bartekn337@gmail.com>
2025-05-30 13:53:17 -0300

    GitHub pages (#8)
    * fix
    
    * fix
    
    * fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  /* config options here */
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
